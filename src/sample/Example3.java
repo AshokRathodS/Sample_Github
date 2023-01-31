6bd94aa2bdc168e1e1b579a760ee8f41e8b1ff76
rasho <rasho@HP>
2023-01-31 10:39:09 +0530

    [Rathod]:New Create Project

---
+++ src/sample/Example3.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Example3 
+{
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Good Day");
+		
+	}
+
+}
---
+++ src/sample/Example5.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class Example5 
+{public static void main(String[] args) 
+{
+	System.out.println("Good Nice day");
+}
+
+}
